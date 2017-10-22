513a7f9494c1ef0931631dec20f3315f3576b4c7
madergaser <madergaser10@gmail.com>
2017-10-21 23:46:02 -0500

    Initial implementation of Inputs.

--- src/Inputs.js
+++ src/Inputs.js
@@ -1,36 +1,57 @@
 /* @flow */
 
 import React from "react";
+import type { Input } from "./runContract";
 
-type InputData = {
-  name: string,
-  t: string // type
-};
-
-const Input = ({ input }: { input: InputData }) => {
+const InputComponent = ({ input, onRemove }: { input: Input, onRemove: any}) => {
   return (
     <div>
       <h3>{input.name}</h3>
-      <p>Remove</p>
+      <button onClick = {onRemove}>Remove</button>
     </div>
   );
 };
 
 type Props = {
-  inputs: Array<InputData>,
+  inputs: Array<Input>,
   addInput: Input => void, // adds an input to the input list
   onRemove: string => void // takes in input name and removes it
 };
 
-const Inputs = ({ inputs }: Props) => {
-  return (
-    <div>
-      {inputs.map(input => <Input input={input} />)}
-      <div>
-        <p>Add input ____</p>
-      </div>
-    </div>
-  );
+type State = {
+  inputName: string
 };
+
+class Inputs extends React.Component<Props, State> {
+
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      inputName: ""
+    };
+  }
+
+  onNameChange = (name: string) => {
+    this.setState({
+      inputName: name
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        {this.props.inputs.map(input => <InputComponent input={input} onRemove={this.props.onRemove} />)}
+        <div>
+          <p>Add Input:</p>
+          <input 
+          name="nameInput"
+          value = {this.state.inputName}
+          onChange = {this.onNameChange}/> 
+          <button onClick = {this.props.addInput}> Add </button>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Inputs;
